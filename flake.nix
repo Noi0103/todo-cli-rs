fb50618c0c740088f9333f8f1c70414b607696b2
Noi0103 <noi0103@proton.me>
2025-09-01 18:15:37 +0200

    checks for rust hang on cache fetch

--- flake.nix
+++ flake.nix
@@ -1,50 +1,105 @@
 {
+  description = "An example project";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
+    systems.url = "github:nix-systems/default";
+    git-hooks.url = "github:cachix/git-hooks.nix";
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      systems,
+      git-hooks,
       ...
-    }:
+    }@inputs:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
-      packages.${system} = {
-        default = self.packages.${system}.todo-cli-rs;
-        todo-cli-rs = pkgs.rustPlatform.buildRustPackage {
-          pname = "todo-cli-rs";
-          version = "1.0";
-          src = pkgs.lib.sourceFilesBySuffices ./. [
-            ".rs"
-            ".toml"
-            ".lock"
-          ];
-          cargoLock = {
-            lockFile = ./Cargo.lock;
+      packages = forEachSystem (system: {
+        default =
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            inherit (self.checks.${system}.pre-commit-check)
+              shellHook
+              enabledPackages
+              ;
+          in
+          pkgs.rustPlatform.buildRustPackage {
+            pname = "todo-cli-rs";
+            version = "1.0";
+            src = pkgs.lib.sourceFilesBySuffices ./. [
+              ".rs"
+              ".toml"
+              ".lock"
+            ];
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+          };
+      });
+
+      # Enter a development shell with `nix develop`.
+      # The hooks will be installed automatically.
+      # Or run pre-commit manually with `nix develop -c pre-commit run --all-files`
+      devShells = forEachSystem (system: {
+        default =
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            inherit (self.checks.${system}.pre-commit-check)
+              shellHook
+              enabledPackages
+              ;
+          in
+          pkgs.mkShell {
+            inherit shellHook;
+            buildInputs = enabledPackages;
+            packages = with pkgs; [
+              rustc
+              cargo
+              rust-analyzer
+              rustfmt
+              clippy
+            ];
+            env = {
+              RUST_BACKTRACE = "full";
+            };
+          };
+      });
+
+      # Run the hooks with `nix fmt`.
+      formatter = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          config = self.checks.${system}.pre-commit-check.config;
+          inherit (config) package configFile;
+          script = ''
+            ${package}/bin/pre-commit run --all-files --config ${configFile}
+          '';
+        in
+        pkgs.writeShellScriptBin "pre-commit-run" script
+      );
+
+      # Run the hooks in a sandbox with `nix flake check`.
+      # Read-only filesystem and no internet access.
+      checks = forEachSystem (system: {
+        pre-commit-check = inputs.git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixfmt-rfc-style.enable = true;
+            #rustfmt.enable = true;
+            #clippy = {
+            #enable = true;
+            #packageOverrides.cargo = pkgs.cargo;
+            #packageOverrides.clippy = pkgs.clippy;
+            #settings.allFeatures = true;
+            #};
           };
         };
-      };
-
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          nixfmt-rfc-style
-
-          # rust
-          rustc
-          cargo
-          rust-analyzer
-          rustfmt
-          clippy
-        ];
-
-        env = {
-          RUST_BACKTRACE = "full";
-        };
-      };
+      });
     };
 }
